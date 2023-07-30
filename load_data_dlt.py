5084a039508b1d3756e2d79ecbb74cb6eda638f1
Abhishek@work <porwal.abhishek.work@gmail.com>
2023-07-30 11:22:20 +0530

    created load_data file. Need to add actual codes!

---
+++ load_data_dlt.py
@@ -0,0 +1,9 @@
+# load files from cloud object storage
+
+# load data from a message bus
+
+# load data from external systems
+
+# load small or static datasets from cloud object storage
+
+# securely access storage credentials with secrets in a pieline
